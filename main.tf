4ec0a6661677ce96e5a6a233414d26fcc0145706
vipinkr23 <vipinkr23@gmail.com>
2025-08-23 16:35:53 +0530

    rg added

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+subscription_id = "8047b6da-5aca-4a1b-a741-1c075ee56791"
+features{}
+}
+
+
+resource "azurerm_resource_group" "rg-vips" {
+  name     = "vipintest"
+  location = "West US"
+  }
+
